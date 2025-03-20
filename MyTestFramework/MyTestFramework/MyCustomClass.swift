713f0cf6dc4b8680d8453884aecf87c2aa85662f
Aakash Narkar <56544770+AakashNarkar@users.noreply.github.com>
2025-03-20 23:14:28 +0530

    Add files via upload
    Initial Commit

---
+++ MyTestFramework/MyTestFramework/MyCustomClass.swift
@@ -0,0 +1,16 @@
+//
+//  MyCustomClass.swift
+//  MyTestFramework
+//
+//  Created by Aakash Narkar on 20/03/25.
+//
+
+import Foundation
+
+public class MyCustomClass {
+    public init() {}
+    
+    public func sayHello() -> String {
+        return "Hello from MyCustomFramework!"
+    }
+}
